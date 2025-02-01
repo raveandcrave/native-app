f4be616693e95ecb5374ae38688703ec6f443ae1
Viktor Kopan <victorkopan98@gmail.com>
2025-02-01 11:20:02 +0500

    RefreshControl

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,12 +1,13 @@
 import { StudentCourseDescription } from '@/entities/course/model/course.model';
 import { courseAtom, loadCourseAtom } from '@/entities/course/model/course.state';
+import { Colors } from '@/shared/tokens';
 import CourseCard from '@/widgets/course/ui/CourseCard/CourseCard';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { useEffect } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, FlatList, RefreshControl, StyleSheet, View } from 'react-native';
 
 export default function MyCourses() {
-  const { courses } = useAtomValue(courseAtom);
+  const { courses, isLoading } = useAtomValue(courseAtom);
   const loadCourses = useSetAtom(loadCourseAtom);
 
   useEffect(() => {
@@ -22,9 +23,24 @@ export default function MyCourses() {
   };
 
   return (
-    courses.length > 0 && (
-      <FlatList data={courses} keyExtractor={(item) => item.id.toString()} renderItem={renderCourse} />
-    )
+    <>
+      {isLoading && <ActivityIndicator size="large" color={Colors.primary} />}
+      {courses.length > 0 && (
+        <FlatList
+          refreshControl={
+            <RefreshControl
+              colors={[Colors.primary]}
+              tintColor={Colors.primary}
+              refreshing={isLoading}
+              onRefresh={loadCourses}
+            />
+          }
+          data={courses}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={renderCourse}
+        />
+      )}
+    </>
   );
 }
 
